c72e3bb63f5000cf01f505a718c761507690c7f4
Majid <132278743+ThunderEon@users.noreply.github.com>
2024-03-03 10:19:07 +0330

    qr ends

---
+++ script.js
@@ -0,0 +1,17 @@
+const input = document.querySelector("form input[type=text]");
+const button = document.querySelector("form button");
+const qrBox = document.querySelector(".qr-code-box");
+const qrImg = document.querySelector(".qr-code-box img");
+
+button.addEventListener("click", () => {
+  let qrValue = input.value;
+  if (!qrValue) return alert("لطفا متن یا آدرس را بنویسید");
+
+  qrImg.src = ` https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`;
+  button.innerHTML = "در حال دریافت QR";
+
+  qrImg.addEventListener("load", () => {
+    qrBox.classList.remove("hidden");
+    button.innerHTML = "تولید QR";
+  });
+});
